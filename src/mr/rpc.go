b4973f6a31b6a8b6edea9cb9924c7708ba13d5c5
QingYuanM <qingyuanm@gitcode.net>
2025-03-02 21:40:42 +0800

    Passed All Tests Of Lab1:MapReduce

--- src/mr/rpc.go
+++ src/mr/rpc.go
@@ -9,10 +9,30 @@ package mr
 import "os"
 import "strconv"
 
-//
 // example to show how to declare the arguments
 // and reply for an RPC.
 //
+// worker call Coordinator for getting a task(map or reduce)
+type GetTaskRequest struct {
+	X int //暂时没用
+}
+type GetTaskResponse struct {
+	MFileName    string   //map文件名字
+	TaskName     string   //任务名字
+	RFileName    []string //reduce文件名字
+	TaskType     int      //0:map,1:reduce,2:sleep
+	ReduceNumber int
+}
+
+// worker send the message about outfile name to Coordinator
+type ReportStatusRequest struct {
+	FilesName []string //告诉master，中间文件的名字，reduce用不上
+	TaskName  string
+	//FileName string//output file name
+}
+type ReportStatusResponse struct {
+	X int
+}
 
 type ExampleArgs struct {
 	X int
@@ -23,7 +43,6 @@ type ExampleReply struct {
 }
 
 // Add your RPC definitions here.
-
 
 // Cook up a unique-ish UNIX-domain socket name
 // in /var/tmp, for the coordinator.
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,48 +1,161 @@
 package mr
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+	"time"
+)
 import "log"
 import "net/rpc"
 import "hash/fnv"
 
-
-//
 // Map functions return a slice of KeyValue.
-//
 type KeyValue struct {
 	Key   string
 	Value string
 }
 
-//
 // use ihash(key) % NReduce to choose the reduce
 // task number for each KeyValue emitted by Map.
-//
 func ihash(key string) int {
 	h := fnv.New32a()
 	h.Write([]byte(key))
 	return int(h.Sum32() & 0x7fffffff)
 }
 
+type ByKey []KeyValue
 
-//
+func (a ByKey) Len() int           { return len(a) }
+func (a ByKey) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByKey) Less(i, j int) bool { return a[i].Key < a[j].Key }
+
+func HandleReduce(reducef func(string, []string) string, filenames []string) string {
+	files := make([]*os.File, len(filenames))
+	intermediate := []KeyValue{}
+	for i := 0; i < len(filenames); i++ {
+		files[i], _ = os.Open(filenames[i])
+		kv := KeyValue{}
+		dec := json.NewDecoder(files[i])
+		for {
+			if err := dec.Decode(&kv); err != nil {
+				break
+			}
+			intermediate = append(intermediate, kv)
+		}
+
+	}
+	sort.Sort(ByKey(intermediate))
+	//log.Println("intermediate",len(intermediate))
+	oname := "mr-out-"
+
+	index := filenames[0][strings.LastIndex(filenames[0], "_")+1:]
+	oname = oname + index
+	ofile, _ := os.Create(oname)
+
+	//
+	// call Reduce on each distinct key in intermediate[],
+	// and print the result to mr-out-0.
+	//
+	i := 0
+	for i < len(intermediate) {
+		j := i + 1
+		for j < len(intermediate) && intermediate[j].Key == intermediate[i].Key {
+			j++
+		}
+		values := []string{}
+		for k := i; k < j; k++ {
+			values = append(values, intermediate[k].Value)
+		}
+		output := reducef(intermediate[i].Key, values)
+
+		// this is the correct format for each line of Reduce output.
+		fmt.Fprintf(ofile, "%v %v\n", intermediate[i].Key, output)
+
+		i = j
+	}
+	return oname
+}
+func HandleMap(mapf func(string, string) []KeyValue, filename string, filenum int, tasknum string) []string {
+	intermediate := []KeyValue{}
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("cannot open %v", filename)
+	}
+	content, err := ioutil.ReadAll(file)
+	//log.Println(content)
+	if err != nil {
+		log.Fatalf("cannot read %v", filename)
+	}
+	file.Close()
+	kva := mapf(filename, string(content))
+	intermediate = append(intermediate, kva...)
+	filenames := make([]string, filenum)
+	files := make([]*os.File, filenum)
+
+	for i := 0; i < filenum; i++ {
+		oname := "mr"
+		oname = oname + "_" + tasknum + "_" + strconv.Itoa(i)
+		//log.Println("create ",oname)
+		ofile, _ := os.Create(oname)
+		files[i] = ofile
+		filenames[i] = oname
+	}
+	for _, kv := range intermediate {
+		index := ihash(kv.Key) % filenum
+		enc := json.NewEncoder(files[index])
+		enc.Encode(&kv)
+	}
+	return filenames
+}
+
 // main/mrworker.go calls this function.
-//
 func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
+	for {
+		args := GetTaskRequest{}
+		args.X = 0
+		rep := GetTaskResponse{}
+		call("Coordinator.GetTask", &args, &rep)
+		//log.Println("name",rep.TaskName,"type",rep.TaskType)
+		if rep.TaskType == Map {
+			filenames := HandleMap(mapf, rep.MFileName, rep.ReduceNumber, rep.TaskName)
+			rargs := ReportStatusRequest{}
+			rargs.TaskName = rep.TaskName
+			rargs.FilesName = filenames
+			rreply := ReportStatusResponse{}
+			rreply.X = 0
+			call("Coordinator.Report", &rargs, &rreply)
+		} else if rep.TaskType == Reduce {
+			HandleReduce(reducef, rep.RFileName)
+			rargs := ReportStatusRequest{}
+			rargs.TaskName = rep.TaskName
+			rargs.FilesName = make([]string, 0)
+			rreply := ReportStatusResponse{}
+			rreply.X = 0
+			call("Coordinator.Report", &rargs, &rreply)
+		} else if rep.TaskType == Sleep {
+			time.Sleep(time.Millisecond * 10)
+			//log.Println("Sleep Task")
+		} else {
+			log.Fatal("get task is not map sleep and reduce")
+		}
 
+	}
 	// Your worker implementation here.
 
-	// uncomment to send the Example RPC to the coordinator.
+	// uncomment to send the Example RPC to the Coordinator.
 	// CallExample()
 
 }
 
-//
 // example function to show how to make an RPC call to the coordinator.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func CallExample() {
 
 	// declare an argument structure.
@@ -61,11 +174,9 @@ func CallExample() {
 	fmt.Printf("reply.Y %v\n", reply.Y)
 }
 
-//
 // send an RPC request to the coordinator, wait for the response.
 // usually returns true.
 // returns false if something goes wrong.
-//
 func call(rpcname string, args interface{}, reply interface{}) bool {
 	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
 	sockname := coordinatorSock()
